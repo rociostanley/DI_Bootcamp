c9455a42c56c3200fa310fd0d129b1ffc5c9324e
Rocio Stanley <rociostanley@Rocios-MacBook-Air.local>
2022-02-10 19:55:28 +0200

    dsld

--- week7/day2/dailychallenge/main.py
+++ week7/day2/dailychallenge/main.py
@@ -1,38 +1,20 @@
-str_test = """
-    7I3
-    Tsi
-    h%x
-    i #
-    sM 
-    $a 
-    #t%
-    ^r!
-"""
+matrix = [
+	["7", "i", "3"],
+	["T", "s", "i"],
+	["h", "%", "x"],
+	["i", " ", "#"],
+	["s", "M", " "],
+	["$", "a", " "],
+	["#", "t", "%"],
+	["^", "r", "!"]
+]
 
-l1 = str_test.split("\n")
+rows_num = len(matrix)
+columns_num = len(matrix[0])
 
-l1.remove('')
+for i in range(columns_num):  # 0, 1, 2
+	for j in range(rows_num):  # 0,1, .. 7
+		if matrix[j][i].isalpha():
+			print(matrix[j][i])
 
-str_arr = ""
-for k in range(0, len(max(l1, key=len))):
-
-    for l in l1:
-        if k > len(l) - 1:
-            break
-
-        if l[k].isalpha():
-            if str(l[k]).isupper() and k != 0:
-                str_arr += ' '
-            str_arr += str(l[k])
-
-print(str_arr)
-for l in l1:
-        if k > len(l) - 1:
-            break
-
-        if l[k].isalpha():
-            if str(l[k]).isupper() and k != 0:
-                str_arr += ' '
-            str_arr += str(l[k])
-            print(str_arr)nt(new_string)
-
+	print('\n')
---
+++ week7/day2/xp/main.py
@@ -0,0 +1,7 @@
+#EX1
+# def display_message():
+#     print('I am learning Python functions')
+
+# display_message()
+
+#EX2
