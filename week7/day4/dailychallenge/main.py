0c5ffedb28cb23dd64ca601ee06d211b69c30271
Rocio Stanley <rociostanley@Rocios-MacBook-Air.local>
2022-02-10 16:23:46 +0200

    msdfas

--- week7/day4/dailychallenge/main.py
+++ week7/day4/dailychallenge/main.py
@@ -1,8 +1,4 @@
 def simple_calc_div(num1=5, num2=0):
-    """
-    Info: Write a function to compute 5/0 and use try/except to catch the exceptions.
-    """
-
     try:
         return num1 / num2
     except ZeroDivisionError:
